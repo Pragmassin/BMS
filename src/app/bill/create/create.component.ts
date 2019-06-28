e6ad8c7a3449a595abb09029f33e530e2e2c2b7b
Dipam Goswami <dipamgoswami01@gmail.com>
2019-06-28 10:56:53 +0530

    V2: Change in routing, Tables and Upload Buttons are functional

--- src/app/bill/create/create.component.ts
+++ src/app/bill/create/create.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
-})
+  styleUrls: ['./create.component.scss']})
 export class CreateComponent implements OnInit {
-
-  constructor() { }
+  posts = [];
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post = {
+      vendor: form.value.vendor,
+      service: form.value.service,
+      date: form.value.date,
+      value: form.value.value,
+      bill: form.value.bill
+    };
+    this.posts.push(post);
+    form.resetForm();
+  }
 
   ngOnInit() {
   }
-
 }
--- src/app/vendormaster/vendormaster.component.ts
+++ src/app/vendormaster/vendormaster.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-vendormaster',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendormasterComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  posts = [];
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post = {
+      name: form.value.user,
+      ad1: form.value.ad1,
+      ad2: form.value.ad2,
+      ad3: form.value.ad3,
+      area: form.value.area,
+      city: form.value.city,
+      state: form.value.state,
+      pin: form.value.pin,
+      email: form.value.email,
+      phone: form.value.phone,
+      contact: form.value.contact,
+      escemail: form.value.esc,
+      escphone: form.value.escph,
+      gst: form.value.gst
+    };
+    this.posts.push(post);
+    form.resetForm();
   }
 
+  constructor() { }
+  ngOnInit() {}
 }
